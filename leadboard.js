c52dcf55cc9d62a0be6854fcdad51c60693ce4d7
Mustapha <b166er.dada@gmail.com>
2020-07-20 01:09:08 +0800

    set player data and update statistic

--- leadboard.js
+++ leadboard.js
@@ -1,7 +1,7 @@
 handlers.updateStatistic = function (args, context) {
   var event = context.playStreamEvent;
 
-  if(event != null) {
+  if (event != null) {
     var request = {
       PlayFabId: currentPlayerId,
       Statistics: [{
@@ -11,26 +11,54 @@ handlers.updateStatistic = function (args, context) {
     };
 
     server.UpdatePlayerStatistics(request);
-  }    
+  }
 };
 
 handlers.updateStatistics = function (args, context) {
   var event = context.playStreamEvent;
 
-  if(event != null) {
+  if (event != null) {
     var stats = event.stats;
     var statArray = [];
-    
-    for(var key in stats) {
-    	var value = stats[key];  
-    	statArray.push({StatisticName: key, Value: value});
-	}
-    
+
+    for (var key in stats) {
+      var value = stats[key];
+      statArray.push({ StatisticName: key, Value: value });
+    }
+
     var request = {
-      PlayFabId: currentPlayerId, 
+      PlayFabId: currentPlayerId,
       Statistics: statArray
     };
-    
+
     server.UpdatePlayerStatistics(request);
-  }    
+  }
 };
+
+handlers.setRecordData = function (args, context) {
+  var event = context.playStreamEvent;
+  var data = event.data;
+  var updateStatistic = event.updateStatistic;
+  var statisticName = event.statisticName;
+  var statisticValue = event.statisticValue;
+
+  if (event != null) {
+    var updateUserDataRequest = {
+      Data: data,
+      PlayFabId: currentPlayerId
+    };
+
+    server.UpdateUserPublisherReadOnlyData(updateUserDataRequest)
+    if (updateStatistic) {
+      var statisticsRequest =
+      {
+        PlayFabId: currentPlayerId,
+        Statistics: [{
+          StatisticName: statisticName,
+          Value: statisticValue
+        }]
+      };
+      server.UpdatePlayerStatistics(statisticsRequest);
+    }
+  }
+}
